db059147e85751dd3ea6ac337e566077a43ee71f
Robert Vilcu <robertvilcu89@gmail.com>
2025-06-01 18:56:31 +0100

    feat: refactor error boundary

---
+++ app/components/error-boundary.tsx
@@ -0,0 +1,46 @@
+import React, { type ReactElement } from 'react'
+import {
+	type ErrorResponse,
+	isRouteErrorResponse,
+	useParams,
+	useRouteError,
+} from 'react-router'
+import { getErrorMessage } from '~/lib/utils'
+
+type StatusHandler = (info: {
+	error: ErrorResponse
+	params: Record<string, string | undefined>
+}) => ReactElement | null
+
+export function GeneralErrorBoundary({
+	defaultStatusHandler = ({ error }) => (
+		<p>
+			{error.status} {error.data}
+		</p>
+	),
+	statusHandlers,
+	unexpectedErrorHandler = (error) => <p>{getErrorMessage(error)}</p>,
+}: {
+	defaultStatusHandler?: StatusHandler
+	statusHandlers?: Record<number, StatusHandler>
+	unexpectedErrorHandler?: (error: unknown) => ReactElement | null
+}) {
+	const error = useRouteError()
+	const params = useParams()
+	const isResponse = isRouteErrorResponse(error)
+
+	if (typeof document !== 'undefined') {
+		console.error(error)
+	}
+
+	return (
+		<div className="text-h2 container flex items-center justify-center p-20">
+			{isResponse
+				? (statusHandlers?.[error.status] ?? defaultStatusHandler)({
+						error,
+						params,
+					})
+				: unexpectedErrorHandler(error)}
+		</div>
+	)
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,11 @@
 import globalStyleSheet from "./styles/global.css?url";
 
-import { data, isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { data, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "react-router";
 import type { Route } from "./+types/root";
 
 import { getPublicEnv } from "./lib/environment/env.common";
 import { APP_DESCRIPTION, APP_NAME } from "./resources/app-config";
+import { GeneralErrorBoundary } from "./components/error-boundary";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -57,28 +58,4 @@ export default function App() {
 	return <Outlet />;
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-	let message = "Oops!";
-	let details = "An unexpected error occurred.";
-	let stack: string | undefined;
-
-	if (isRouteErrorResponse(error)) {
-		message = error.status === 404 ? "404" : "Error";
-		details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
-	} else if (import.meta.env.DEV && error && error instanceof Error) {
-		details = error.message;
-		stack = error.stack;
-	}
-
-	return (
-		<main className="container mx-auto p-4 pt-16">
-			<h1>{message}</h1>
-			<p>{details}</p>
-			{stack && (
-				<pre className="w-full overflow-x-auto p-4">
-					<code>{stack}</code>
-				</pre>
-			)}
-		</main>
-	);
-}
+export const ErrorBoundary = GeneralErrorBoundary;
