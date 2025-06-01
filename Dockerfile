52a8eb4ab9b7a53304d196b7decb30fcff9c15c8
Robert Vilcu <robertvilcu89@gmail.com>
2025-06-01 18:13:17 +0100

    chore: init

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,35 @@
-FROM node:20-alpine AS development-dependencies-env
+FROM oven/bun:1 AS dependencies-env
+RUN bun i -g cross-env
+
 COPY . /app
-WORKDIR /app
-RUN npm ci
 
-FROM node:20-alpine AS production-dependencies-env
-COPY ./package.json package-lock.json /app/
-WORKDIR /app
-RUN npm ci --omit=dev
+# [optional] tests & build
+ENV NODE_ENV=production
 
-FROM node:20-alpine AS build-env
-COPY . /app/
+ARG VITE_API_URL
+ARG VITE_AUTH_API_URL
+ENV VITE_API_URL=${VITE_API_URL}
+ENV VITE_AUTH_API_URL=${VITE_AUTH_API_URL}
+
+FROM dependencies-env AS development-dependencies-env
+COPY ./package.json bun.lock /app/
+WORKDIR /app
+RUN bun i --frozen-lockfile
+
+FROM dependencies-env AS production-dependencies-env
+COPY ./package.json bun.lock /app/
+WORKDIR /app
+RUN bun i --production
+
+FROM dependencies-env AS build-env
+COPY ./package.json bun.lock /app/
 COPY --from=development-dependencies-env /app/node_modules /app/node_modules
 WORKDIR /app
-RUN npm run build
+RUN bun run build
 
-FROM node:20-alpine
-COPY ./package.json package-lock.json /app/
+FROM dependencies-env
+COPY ./package.json bun.lock /app/
 COPY --from=production-dependencies-env /app/node_modules /app/node_modules
 COPY --from=build-env /app/build /app/build
 WORKDIR /app
-CMD ["npm", "run", "start"]
+CMD ["bun", "run", "start"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,64 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import path from "path";
+import { reactRouterDevTools } from "react-router-devtools";
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { defineConfig } from "vite";
+
+const devConfig = {
+	server: {
+		port: 3000,
+		cors: true,
+	},
+
+	plugins: [
+		reactRouterDevTools(),
+		reactRouter(),
+		tsconfigPaths(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			babelConfig: {
+				presets: ["@babel/preset-typescript"], // if you use TypeScript
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+		// @ts-ignore
+		tailwindcss(),
+	],
+
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./app"),
+		},
+	},
+};
+
+const prodConfig = {
+	plugins: [
+		reactRouter(),
+		tsconfigPaths(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			babelConfig: {
+				presets: ["@babel/preset-typescript"], // if you use TypeScript
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+		// @ts-ignore
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"react-dom/server": "react-dom/server.node",
+			"~": path.resolve(__dirname, "./app"),
+		},
+	},
+};
+
+const config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	...config,
 });
